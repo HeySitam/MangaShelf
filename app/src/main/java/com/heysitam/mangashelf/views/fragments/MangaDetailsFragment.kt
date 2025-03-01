e23ebd66f0fa6e617eee199d1d5a88e6d8037458
Sitam Sardar <sitamadex11@gmail.com>
2025-03-01 09:41:26 +0530

    First Commit and Initial Setup

---
+++ app/src/main/java/com/heysitam/mangashelf/views/fragments/MangaDetailsFragment.kt
@@ -0,0 +1,20 @@
+package com.heysitam.mangashelf.views.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.heysitam.mangashelf.R
+
+
+class MangaDetailsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_manga_details, container, false)
+    }
+}
---
+++ app/src/main/java/com/heysitam/mangashelf/views/fragments/MangaListFragment.kt
@@ -0,0 +1,18 @@
+package com.heysitam.mangashelf.views.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.heysitam.mangashelf.R
+
+class MangaListFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_manga_list, container, false)
+    }
+}
